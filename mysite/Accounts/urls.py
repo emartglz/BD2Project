446dbf8658814f67300c5c04be346ec94b8a36c5
kikeXD <kike@gamil.com>
2020-10-05 11:52:05 -0400

    removing testing stuff from Accounts

--- mysite/Accounts/urls.py
+++ mysite/Accounts/urls.py
@@ -5,6 +5,4 @@ app_name = 'Accounts'
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
 ]
--- mysite/Accounts/views.py
+++ mysite/Accounts/views.py
@@ -14,21 +14,4 @@ def signup_view(request):
     
     else:
         form = UserCreationForm()
-    return render(request, 'Accounts/signup.html', {'form': form}) 
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')
-
-    else:
-        form = AuthenticationForm()
-    return render(request, 'Accounts/login.html', {'form': form})
-
-def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('home')
+    return render(request, 'Accounts/signup.html', {'form': form})

ce242a57382a2826e21af9a1c0fe7debc1031430
kikeXD <kike@gamil.com>
2020-10-05 11:51:17 -0400

    removing test stuff from BD2Project

--- mysite/BD2Project/views.py
+++ mysite/BD2Project/views.py
@@ -1,20 +1,3 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
-@login_required(login_url="/Accounts/login")
-def testing_auth(request):
-    return render(request, 'BD2Project/auth_test.html')
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
